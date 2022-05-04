c88768d9d6263e345aa49cda723cb3ba4c56b781
aryan02420 <aryan02420@gmail.com>
2022-05-04 20:08:45 +0000

    fix: output colors

--- test/run.sh
+++ test/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if diff ./test/expected ./test/result >/dev/null ; then
-  printf "\x1b[92mALL TESTS PASS\x1b[0m"
+  printf "\e[32mALL TESTS PASS\n\e[0m"
 else
-  printf "\x1b[91mSOME TEST FAILED\x1b[0m"
+  printf "\e[32mSOME TEST FAILED\n\e[0m"
 fi
